7740c0acb1f9d15ff7dcd125fda8e276c3641380
ProfAndersonVanin <ProfAndersonVanin@gmail.com>
2024-08-01 11:21:42 -0300

    Primeira Versão do EtecFlix

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>ETEC MCM</h1>
+      <p>Teste de React</p>
+    </>
+  );
+}
+
+export default App;
