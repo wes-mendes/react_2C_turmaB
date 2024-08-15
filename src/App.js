bb03b4e54d1faf7fbfac52a6e519361c281ea286
wes-mendes <wesleymendesdesousa7@gmail.com>
2024-08-15 08:35:14 -0300

    2024

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
-      <Header/>
-      <Banner/>
-      <h1>ETEC MCM</h1>
-      <p>Teste de React</p>
-      <Footer/>
+      <Header />
+      <Banner image="home" />
+      <Container>
+        <h1>Hello World! Olá Mundo</h1>
+        <p>Estou aprendendo React JS!</p>
+      </Container>
+      <Footer />
     </>
   );
 }
